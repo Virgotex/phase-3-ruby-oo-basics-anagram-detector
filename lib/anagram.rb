506359d12d219066ae69d7abee04411aaf8517f3
Carlton Kibe <Virgotex15@gmail.com>
2023-05-20 01:53:48 +0300

    Completed Asssignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    words.select { |w| w.downcase.chars.sort == word.downcase.chars.sort }
+  end
+end
